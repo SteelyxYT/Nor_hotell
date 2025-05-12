1906fe7d6e4db7d0f39652fa770f8e62bfd95620
SteelyxYT <91285184+SteelyxYT@users.noreply.github.com>
2025-05-12 10:07:55 +0200

    Started adding routing

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,3 +1,6 @@
+using System.Net.NetworkInformation;
+using AuthAPI.interfaces.routes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +24,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+UserRoutes.Route(app);
 
 app.Run();
---
+++ AuthAPI/interfaces/routes/UserRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthAPI.interfaces.routes
+{
+    public class UserRoutes
+    {
+        public static void Route(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/v1/users", () => "Get all users");
+            app.MapGet("/api/v1/users/{id}", () => "Get user by id {id}");
+            app.MapPost("/api/v1/users", () => "Create user");
+            app.MapPut("/api/v1/users/{id}", () => "Update user");
+            app.MapDelete("/api/v1/users/{id}", () => "Delete user");
+        }
+    }
+}
